1183f4a19eebf23022e27aae7e95cc6707e5d24b
sabbir322 <sabbirhosan370@gmail.com>
2022-05-08 18:03:29 +0600

    secure user name and password

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 
@@ -21,18 +22,28 @@ app.get('/', (req, res) => {
 // stackoverflow anwser: https://stackoverflow.com/questions/72157598/is-their-storage-limit-in-github-repository-storage/72157616#72157616s
     
 
-const uri = `mongodb+srv://warehouse1:l9XpzbSczUMLzNAR@warehouseproduct.bux0i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@warehouseproduct.bux0i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
   try{
     await client.connect();
     const shoesCollection = client.db("warehouseProduct").collection("shoes");
     // show product 
+    console.log('data base is connectedS')
     app.get('/shoes', async(req, res) => {
         const query = {};
         const cursor = shoesCollection.find(query)
         const result = await cursor.toArray()
         res.json(result)
+    })
+    // my product 
+    app.get('/product', async(req, res) => {
+      const email = req.query.email;
+      console.log(email)
+      const query = { email: email };
+      const cursor = shoesCollection.find(query);
+      const result = await cursor.toArray();
+      res.json(result)
     })
     // single product
     app.get('/shoes/:id', async(req, res) => {
@@ -41,6 +52,13 @@ async function run() {
       const result = await shoesCollection.findOne(quary);
       res.json(result)
     })
+    // add a new product
+    app.post('/shoes', async(req, res) => {
+      const product = req.body;
+      const result = await shoesCollection.insertOne(product);
+      res.json(result);
+    })
+    // update user
     app.put('/shoes/:id', async(req, res) => {
       const quantity = req.body.totalQuantity;
       const sold = req.body.totalSold;
